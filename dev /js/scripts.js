c7e101758594e19af36ea3a3a88e81ae5c864fbe
Julia Kimball <juliakimball6@gmail.com>
2021-06-08 20:28:43 -0400

    more changes
    more changes

--- dev /js/scripts.js
+++ dev /js/scripts.js
@@ -1,1 +1,18 @@
+import $ from "jquery";
 
+console.log("hello");
+
+let $nav = $('nav[data-nav]');
+let isVisible = false;
+
+$(".nav-btns").on("click", function(){
+    console. log("click");
+    if(isVisible === false){
+        $nav.show();
+        isVisible = true;
+    }
+    else{
+        $nav.hide();
+        isVisible = false;
+    }
+})
